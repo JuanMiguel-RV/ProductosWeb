baa2ca43e4fa1253fabaa566280f7a73d5e9a5aa
JuanMiguel-RV <jm_ramirezv@hotmail.com>
2021-10-15 16:42:32 -0500

    Proyecto WEB Productos

---
+++ EntityFramework/Data/EFClientsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Models;
+
+namespace EntityFramework.Data
+{
+    public class EFClientsContext : DbContext
+    {
+        public EFClientsContext (DbContextOptions<EFClientsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFramework.Models.Producto> Products { get; set; }
+    }
+}
---
+++ EntityFramework/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int idProducto { get; set; }
+        public string nombre { get; set; }
+        public string categoria { get; set; }
+        public float precio { get; set; }
+        public int cantidad { get; set; }
+        public string descricpcion { get; set; }
+
+    }
+}
